dd359bcc03bc05563f9fb7cf32a5e141cb0b8cfb
Nenad <nesani@ymail.com>
2023-03-07 19:31:37 +0100

    fundementals part 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+
+function Greeting(){
+    return(
+        <div>
+          <Person />
+          <Message />
+        </div>
+    )
+};
+
+const Person = () => <h2> Tihomir Ilic </h2>;
+const Message = () =>{
+ return (
+    <p> ovo je moja drama</p>
+ )  
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Greeting />)
