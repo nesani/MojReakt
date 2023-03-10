6de85916735df8dd4f70e8b342085014bef87e62
Nenad <nesani@ymail.com>
2023-03-10 19:04:36 +0100

    added inline style

--- src/index.js
+++ src/index.js
@@ -24,6 +24,11 @@ const Book = () =>{
 
 const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg" alt="Atomic Habits" />
 const Title = () => <h2>Atomic Habits</h2>
-const Author = () => <h2>James Clear</h2>
+const Author = () =>
+{
+  const innerStyle = {color:'#617d98', fontsize:'0.75re', marginTop:'0.5rem'}
+  return(
+   <h4 style={innerStyle}>James Clear</h4>
+   )}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Booklist />)
