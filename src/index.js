a292cc8d65bbb75429027a1eed84a79e8206cef1
Nenad <nesani@ymail.com>
2023-03-10 19:22:38 +0100

    refactored book

--- src/index.js
+++ src/index.js
@@ -12,23 +12,17 @@ return <section className="booklist">
 </section>
 }
 
+const author ='James Clear'
 const Book = () =>{
+  const title = 'Atomic Habits'
   return(
   <article className="book">
-  <Image />
-  <Title />
-  <Author />
+     <img src="https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg" alt="Atomic Habits" />
+     <h2>{title}</h2>
+     <h4>{author}</h4>
   </article>
   );
 }
 
-const Image = () => <img src="https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg" alt="Atomic Habits" />
-const Title = () => <h2>Atomic Habits</h2>
-const Author = () =>
-{
-  const innerStyle = {color:'#617d98', fontsize:'0.75re', marginTop:'0.5rem'}
-  return(
-   <h4 style={innerStyle}>James Clear</h4>
-   )}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Booklist />)
