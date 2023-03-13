36d44cf4dfbc31aa738d82ff14506895eb648546
Nenad <nesani@ymail.com>
2023-03-13 18:12:05 +0100

    added events

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const books = [
 
 const Booklist = () =>{
 return <section className="booklist">
+<EventExamples />
 {books.map((books)=> {
   return (
     <Book book={books} key={books.id}/>
@@ -26,9 +27,28 @@ return <section className="booklist">
 })}
 </section>
 }
- 
-const Book = ({book:{img, title, author}}) =>{
+const EventExamples = () =>{
+ const handleFormInput = () =>
+ {
+  console.log('form input');
+ }
+ const handleButtonClick = () =>
+ {
+  alert('buttonclick')
+ }
 
+  return( <section>
+    <form>
+      <h2>Typical form</h2>
+      <input onChange={handleFormInput} type='text' name ='example' style={{margin:'1rem 0'}}/>
+    </form>
+    <button onClick={handleButtonClick}> click me</button>
+  </section>
+  
+  )}
+
+
+const Book = ({book:{img, title, author}}) =>{
   return(
   <article className="book">
      <img src={img} alt={title} />
