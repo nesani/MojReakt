d4491ba559185410e59debe04d3ce3ee7dd13ba9
Nenad <nesani@ymail.com>
2023-03-23 18:20:43 +0100

    added books

---
+++ src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+    {
+      author:'James Clear',
+      title: 'Atomic Habits',
+      img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+      id: 1
+    },
+    {
+      author:'Jordan Moore',
+      title: 'Interesting Facts for Curios Minds',
+      img: 'https://m.media-amazon.com/images/I/518z5dvykPL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+      id: 2
+    }
+  ];
--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import './index.css'
-const books = [
-  {
-    author:'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
-    id: 1
-  },
-  {
-    author:'Jordan Moore',
-    title: 'Interesting Facts for Curios Minds',
-    img: 'https://m.media-amazon.com/images/I/518z5dvykPL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
-    id: 2
-  }
-];
 
+import './index.css'
+import { books } from './books';
+import Book from './book';
 
 const Booklist = () =>{
 
@@ -28,17 +16,6 @@ return <section className="booklist">
 </section>
 }
 
-const Book = (props) =>{
-const {img, title, author, id} = props
-
-  return(
-  <article className="book">
-     <img src={img} alt={title} />
-     <h2>{title}</h2>
-     <h4>{author}</h4>
-  </article>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Booklist />)
