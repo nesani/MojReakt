2d56fa7d06370e132e708e111f4278596dc1729b
Nenad <nesani@ymail.com>
2023-03-23 18:39:33 +0100

    added images

--- src/books.js
+++ src/books.js
@@ -1,14 +1,25 @@
+
+import img1 from "./Images/AtomicHabits.jpg"
+import img2 from "./Images/interestingFacts.jpg"
+import img3 from "./Images/ABriefHistoryOfTime.jpg"
+
 export const books = [
     {
       author:'James Clear',
       title: 'Atomic Habits',
-      img: 'https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL600_SR600,400_.jpg',
+      img: img1,
       id: 1
     },
     {
       author:'Jordan Moore',
       title: 'Interesting Facts for Curios Minds',
-      img: 'https://m.media-amazon.com/images/I/518z5dvykPL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+      img: img2,
       id: 2
+    },
+    {
+      author:'Stephen Hawking',
+      title: 'A Brief history of Time',
+      img: img3,
+      id: 3
     }
   ];
